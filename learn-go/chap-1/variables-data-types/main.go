5f694ab2076fd2f466bfcab6ee7dec32c2ee3646
Shreethaar <shreethaara380@gmail.com>
2024-11-12 09:41:28 +0000

    phase 1 confusion

---
+++ learn-go/chap-1/variables-data-types/main.go
@@ -0,0 +1,21 @@
+package main
+import "fmt"
+
+func main() {
+    /*var foo string
+    var foo string = "Go is awesome"
+
+    var foo, bar string = "Hello", "World"
+    var (
+        foo string = "Hello"
+        bar string = "World"
+    )
+    var foo = "What's my type?"
+    */
+    var i, j int = 1, 2
+    k := 3
+    c, python, java := true, false, "no!"
+    fmt.Println(i,j,k,c,python,java)
+}
+
+
