171d618a831b1dd149d5893658863ab026e5abb2
Shreethaar <shreethaara380@gmail.com>
2024-11-12 11:14:20 +0000

    git tuah push on that thang

---
+++ learn-go/chap-1/variables-and-constants/variable-declaration.go
@@ -0,0 +1,34 @@
+package main
+import ("fmt")
+
+var d int
+var e int = 2
+var f = 3
+
+
+func main() {
+    var student1 string = "John"
+    var student2 string = "Jane"
+    x := 2
+    fmt.Println(student1)
+    fmt.Println(student2)
+    fmt.Println(x)
+
+    var a string 
+    var b int
+    var c bool
+    fmt.Println(a)
+    fmt.Println(b)
+    fmt.Println(c)
+    
+    d = 1
+    fmt.Println(d)
+    fmt.Println(e)
+    fmt.Println(f)
+
+    var one,two,three,four int = 1,3,5,7
+    fmt.Println(one)
+    fmt.Println(two)
+    fmt.Println(three)
+    fmt.Println(four)
+}
