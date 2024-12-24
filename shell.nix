b7e437e1725d8fdd833dd645cbd4743a1dedcc5a
shreethaar <shreethaara380@gmail.com>
2024-12-24 12:41:13 +0100

    i m use nix btw

---
+++ shell.nix
@@ -0,0 +1,24 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+	buildInputs = with pkgs; [
+	binutils
+	coreutils
+	gnumake
+	gcc
+	pkg-config
+	cmake
+	autoconf
+	libtool
+	cargo
+	rustc
+	rustfmt
+	rust-analyzer
+	clippy
+	];
+
+	shellHook = ''
+		echo "Rust version: $(rustc --version)"
+		export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/library"
+		'';
+}
