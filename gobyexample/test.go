c33a05c1f98e59e5d8f85ba39312dc7a33f01629
shreethaar <shreethaara380@gmail.com>
2025-02-18 22:43:24 +0800

    go-routine && errors

---
+++ gobyexample/test.go
@@ -0,0 +1,19 @@
+package main
+import ("fmt")
+
+func main() {
+    const a int = 1
+    // a = 2 
+    var b int = 2
+    fmt.Println(a)
+    fmt.Println(b)
+    
+    b = 3
+    fmt.Println(b)
+ 
+    c := 4
+    fmt.Println(c)
+    c=5
+    fmt.Println(c)
+}
+
